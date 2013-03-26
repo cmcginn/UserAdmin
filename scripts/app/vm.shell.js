a7decf55417e28558f56598d6236001290dd255c
Chris McGinn <chris.s.mcginn@live.com>
2013-03-26 07:39:08 -0400

    Dataservices starting up, no data yet

---
+++ scripts/app/vm.shell.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Chris
+ * Date: 3/26/13
+ * Time: 6:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ scripts/app/vm.users.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Chris
+ * Date: 3/26/13
+ * Time: 6:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
