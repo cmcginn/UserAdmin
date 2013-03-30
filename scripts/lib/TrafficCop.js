084e5049278924e3602b93690d76010f3ffb195a
Chris McGinn <chris.s.mcginn@live.com>
2013-03-30 05:31:41 -0400

    Before switching to simpler architecture

--- scripts/lib/TrafficCop.js
+++ scripts/lib/TrafficCop.js
@@ -1,7 +1,7 @@
 /**
  * Created with JetBrains WebStorm.
  * User: Chris
- * Date: 3/24/13
- * Time: 11:51 AM
+ * Date: 3/28/13
+ * Time: 7:08 AM
  * To change this template use File | Settings | File Templates.
  */
